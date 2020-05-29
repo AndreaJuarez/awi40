5c7d6845008134901cd0c1996fba8e8a6fcd6060
AndreaJuarez1 <1718110394@utectulancingo.edu.mx>
2020-05-29 19:54:18 +0000

    Programa semana 3 funcionando

--- semana_3/mvc/controllers/cookie.py
+++ semana_3/mvc/controllers/cookie.py
@@ -15,7 +15,7 @@ class Cookie:
             web.setcookie("nombre", nombre,expires="",domain=None)
 
           else:
-            nombre = "Predeterminado" #Si no recibe nada en el parametro de nombre, será NA
+            nombre = "Anonimo" #Si no recibe nada en el parametro de nombre, será NA
             web.setcookie("nombre", nombre,expires="",domain=None)
 
           #Condición para el número de visitas
